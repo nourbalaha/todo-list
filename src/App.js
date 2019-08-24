0020e4be3ba98d58759f92e02bd25cc93b0d2ab8
nourbalaha <nourbalaha0@gmail.com>
2019-08-24 20:35:06 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,72 +1,70 @@
-import React, { Component } from 'react'
-import './App.css'
-
+import React, { Component } from "react";
+import "./App.css";
+import Item from "./Item";
 export default class App extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      toDo: '',
-      toDos: []
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.handleDelete = this.handleDelete.bind(this)
+      toDo: "",
+      toDos: [],
+      index: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
-  handleChange (event) {
+  handleChange(event) {
     this.setState({
       toDo: event.target.value
-    })
+    });
   }
-  handleClick (event) {
+  handleClick(event) {
     event.preventDefault();
     this.setState({
       toDos: [...this.state.toDos, this.state.toDo],
-      toDo: ''
-    })
+      toDo: ""
+    });
   }
-  handleDelete(){
-    console.log();
+  handleDelete(i) {
+    console.log("i");
   }
-  render () {
-    const list = this.state.toDos.map((item, i) =>
-      <div class="row">
-        <div class="col-sm-10">
-      <li class='list-group-item' key={i}>
-        {' '}{item}{' '}
-      </li>
-        </div>
-        <div class="col-sm-2">
-      <input class="btn btn-danger" onClick={this.handleDelete} type="button" value="x" />
-      </div>
-      </div>
-    )
+  render() {
+    const list = this.state.toDos.map(
+      (item, i) => {
+        return(<div>
+          <Item todo={item} index={i} key={i} />
+          <input value="x" type="button" onClick={this.handleDelete}></input>
+        </div>)
+
+      }
+    );
     return (
-      <div class='container'>
+      <div class="container">
         <h1 class="text-center m-3">Todo</h1>
         <form onSubmit={this.handleClick}>
-        <div class='row'>
-          <div class='col-sm-8'>
-            <input
-              class='form-control'
-              type='text'
-              value={this.state.toDo}
-              onChange={this.handleChange}
-            />{' '}
-          </div>
-          <div class='col-sm-4'>
-            <input
-              class='btn btn-primary btn-block'
-              type='submit'
-              value='add'
-            />{' '}
-          </div>
-        </div>{' '}
+          <div class="row">
+            <div class="col-sm-8">
+              <input
+                class="form-control"
+                type="text"
+                value={this.state.toDo}
+                onChange={this.handleChange}
+              />{" "}
+            </div>
+            <div class="col-sm-4">
+              <input
+                class="btn btn-primary btn-block"
+                type="submit"
+                value="add"
+              />{" "}
+            </div>
+          </div>{" "}
         </form>
         <div class="container mt-3">
-        <p class="text-center"> To Do List: </p> <ul class='list-group'> {list} </ul>{' '}
+          <p class="text-center"> To Do List: </p>{" "}
+          <ul class="list-group"> {list} </ul>{" "}
         </div>
       </div>
-    )
+    );
   }
 }
-
--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,32 @@
-export default function Item() {
+import React, { Component } from 'react'
+
+export default class Item extends Component {
+  constructor(props){
+    super(props)
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleDelete(){
+    console.log(this.props.index);
+  }
+
+  render() {
     return (
-      <div>
-        
+      <div class="row">
+      <div class="col-sm-10">
+        <li class="list-group-item">
+          {" "}
+          {this.props.todo}{" "}
+        </li>
       </div>
+      <div class="col-sm-2">
+        <input
+          class="btn btn-danger"
+          onClick={this.handleDelete}
+          type="button"
+          value="x"
+        />
+      </div>
+    </div>
     )
   }
+}
