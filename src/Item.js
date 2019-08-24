9405907841415060b75bb466afd0b5bc5846f30e
nourbalaha <nourbalaha0@gmail.com>
2019-08-25 03:42:31 +0900

    wip: deleting each todo

--- src/Item.js
+++ src/Item.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Item extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: this.props.display
+    };
     this.handleDelete = this.handleDelete.bind(this);
   }
-  handleDelete(){
-    console.log(this.props.index);
+  handleDelete() {
+    this.setState({
+      display: false
+    });
   }
 
   render() {
-    return (
-      <div class="row">
-      <div class="col-sm-10">
-        <li class="list-group-item">
-          {" "}
-          {this.props.todo}{" "}
-        </li>
-      </div>
-      <div class="col-sm-2">
-        <input
-          className="btn btn-danger"
-          onClick={this.handleDelete}
-          type="button"
-          value="x"
-        />
-      </div>
-    </div>
-    )
+    if (this.state.display) {
+      return (
+        <div className="row">
+          <div className="col-sm-10">
+            <li className="list-group-item"> {this.props.todo} </li>
+          </div>
+          <div className="col-sm-2">
+            <input
+              className="btn btn-danger"
+              onClick={this.handleDelete}
+              type="button"
+              value="x"
+            />
+          </div>
+        </div>
+      );
+    }else{
+      return <div ></div>
+    }
   }
 }
