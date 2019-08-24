48af6d7e0c8371d897b696d0576cc7e2d55812fe
Nour Balaha <nourbalaha0@gmail.com>
2019-08-24 18:45:44 +0900

    wip: todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      toDo: '',
+      toDos: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+  handleChange (event) {
+    this.setState({
+      toDo: event.target.value
+    })
+  }
+  handleClick (event) {
+    event.preventDefault();
+    this.setState({
+      toDos: [...this.state.toDos, this.state.toDo],
+      toDo: ''
+    })
+  }
+  handleDelete(){
+    console.log();
+  }
+  render () {
+    const list = this.state.toDos.map((item, i) =>
+      <div class="row">
+        <div class="col-sm-10">
+      <li class='list-group-item' key={i}>
+        {' '}{item}{' '}
+      </li>
+        </div>
+        <div class="col-sm-2">
+      <input class="btn btn-danger" onClick={this.handleDelete} type="button" value="x" />
+      </div>
+      </div>
+    )
+    return (
+      <div class='container'>
+        <h1 class="text-center m-3">Todo</h1>
+        <form onSubmit={this.handleClick}>
+        <div class='row'>
+          <div class='col-sm-8'>
+            <input
+              class='form-control'
+              type='text'
+              value={this.state.toDo}
+              onChange={this.handleChange}
+            />{' '}
+          </div>
+          <div class='col-sm-4'>
+            <input
+              class='btn btn-primary btn-block'
+              type='submit'
+              value='add'
+            />{' '}
+          </div>
+        </div>{' '}
+        </form>
+        <div class="container mt-3">
+        <p class="text-center"> To Do List: </p> <ul class='list-group'> {list} </ul>{' '}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
