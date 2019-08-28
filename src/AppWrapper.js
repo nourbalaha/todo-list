a9b84c8f1f926494ff5da333d3844e6b60bd5068
Nour Balaha <nourbalaha0@gmail.com>
2019-08-29 08:09:01 +0900

    wip react-redux

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,5 +1,7 @@
-import Redux from 'redux'
+import React, { Component } from 'react'
+import {createStore} from 'redux'
 import { Provider, connect } from 'react-redux'
+import App from './App';
 
 const ADD_TODO = 'ADD_TODO'
 
@@ -19,7 +21,7 @@ const addToDo = todo => {
   }
 }
 
-const store = Redux.createStore(todoReducer)
+const store = createStore(todoReducer)
 
 const mapStateToProps = state => {
   return {
@@ -32,5 +34,21 @@ const mapDispatchToProps = dispatch => {
     add: function (todo) {
       dispatch(addToDo(todo))
     }
+  }
+}
+
+const Container = connect(mapStateToProps,mapDispatchToProps)(App);
+
+export default class AppWrapper extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return (
+      <Provider store={store}>
+        <Container />
+      </Provider>
+    )
   }
 }
